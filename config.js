1f1396d7ed53f59b66e7fcb6a2cd1173fbd0f091
HedaraOzillS <82649017+HedaraOzillS@users.noreply.github.com>
2021-11-19 17:43:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,32 +1,6 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
-global.Owner = {
-  '6281262474791', '6282248192917': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
-    isDev: false, // if true this number will send if bot have bug and developer mode true
-    isReport: false, // if true this number will send if user use command /report or /join
-    isCreator: true // if true this number will send if user use command /owner or /creator
-  },
-  '6281390658325': { // put your number here
-    isDev: true, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
-    isCreator: false // if true this number will send if user use command /owner or /creator
-  },
-  '6282256115584': { // put your number here
-    name: 'Dave',
-    isDev: false, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
-    isCreator: false // if true this number will send if user use command /owner or /creator
-  },
-  '60199782326': {
-    name: 'Adiixyz',
-    isReport: true, // if true this number will send if user use command /report or /join
-  },
-  '6285713964963': {
-    isReport: true
-  },
-  '13479805233': {} // ignore if you don't want anything, like name dll
-}
+global.Owner = ['62 81262474791', '6282248192917']
 global.mods = ['13479805233'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
