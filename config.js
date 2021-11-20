54796655f183c62d72c74c9b66d05ca1f0147aec
HedaraOzillS <82649017+HedaraOzillS@users.noreply.github.com>
2021-11-20 14:24:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,20 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
-global.Owner = ['62 81262474791', '6282248192917']
+global.Owner = {
+  // use the phone number with the country code, for example Indonesia '62' America '1' etc
+  '6281262474791': { // put your number here
+    name: 'Owner Felix (utama)',
+    isDev: false, // if true this number will send if bot have bug and developer mode true
+    isReport: false, // if true this number will send if user use command /report or /join
+    isCreator: true // if true this number will send if user use command /owner or /creator
+  },
+  '6282248192917': { // put your number here
+    name: 'Khael',
+    isDev: false, // if true this number will send if bot have bug and developer mode true
+    isReport: true, // if true this number will send if user use command /report or /join
+    isCreator: false // if true this number will send if user use command /owner or /creator
+  },
+}
 global.mods = ['13479805233'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -26,7 +40,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
+global.packname = 'wa.me/+62 812-6247-4791'
 global.author = 'Metro Bot Is The Best'
 
 global.multiplier = 39 // The higher, The harder levelup
